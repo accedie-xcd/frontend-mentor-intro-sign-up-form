9c86348dc92228bae8f6642d8f6050635e1df81a
accedie-xcd <accedie.xcd@gmail.com>
2023-01-30 21:51:16 +0800

    update

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,14 +1,26 @@
-const fName = document.forms["form"]["fName"].value;
-const lName = document.getElementById("lName");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const submitBtn = document.getElementById("submitBtn");
 
-submitBtn.addEventListener('click', () => {
-  check();
-})
 
-function validate(){
-  if (fName == "");
-    alert(" ");
+function validate() {
+  alert("asd");
+  let fName = document.getElementById("fName").value;
+  let lName = document.getElementById("lName").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  if (fName === "") {
+    document.getElementById("fName-help").classList.remove("hidden");
+  }
+
+  if (lName === "") {
+    document.getElementById("lName-help").classList.remove("hidden");
+  }
+
+  if (email === "") {
+    document.getElementById("email-help").classList.remove("hidden");
+  }
+
+  if (password === "") {
+    document.getElementById("password-help").classList.remove("hidden");
+  }
+
 }
